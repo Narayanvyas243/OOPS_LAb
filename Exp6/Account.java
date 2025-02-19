92a09df16a353d060a4dfd667d575243f7e6ae3b
Narayanvyas243 <152839463+Narayanvyas243@users.noreply.github.com>
2025-02-19 16:29:44 +0530

    Add files via upload

---
+++ Exp6/Account.java
@@ -0,0 +1,6 @@
+package balance;
+public class Account{
+    public  void display_balance(){
+        System.out.print("Account Balance is : 5000");
+    } 
+}
---
+++ Exp6/ImportPackages.java
@@ -0,0 +1,8 @@
+ package balance;
+public class ImportPackages {
+    public static void main(String [] args){
+        Account a = new Account();
+        a.display_balance();
+    }
+    
+}
