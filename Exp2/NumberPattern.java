3bd6b25c2efe3a86255453c2321ba494d0ce4f32
Narayanvyas243 <152839463+Narayanvyas243@users.noreply.github.com>
2025-03-17 16:27:40 +0530

    Add files via upload

---
+++ Exp2/NumberPattern.java
@@ -0,0 +1,18 @@
+public class NumberPattern {
+
+    public static void main(String[] args) {
+         
+
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            if (i > 1) { 
+                for (int k = i - 1; k >= 1; k--) {
+                    System.out.print(" "); 
+                }
+            }
+            System.out.println(); 
+        }
+    }
+}
