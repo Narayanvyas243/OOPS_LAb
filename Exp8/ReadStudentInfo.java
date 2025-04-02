4e2b29f231598fe3540fccec02e81d60c00ac3e5
Narayanvyas243 <152839463+Narayanvyas243@users.noreply.github.com>
2025-04-02 16:26:40 +0530

    Add files via upload

---
+++ Exp8/ReadStudentInfo.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+
+public class ReadStudentInfo {
+
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("student.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
